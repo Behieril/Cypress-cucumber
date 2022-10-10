b06387314fbbdd3f11ebdac37b223ae603cad330
Bobkov Roman <roman.bobokov@gmail.com>
2022-10-10 23:01:27 +0300

    Code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Cypress-cucumber
+# Cypress-cucumber-task
+There is 15 test cases in Cypress + Cucumber framework
---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+        defaultCommandTimeout: 10000
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    waitForAnimations: true,  
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://www.google.com",
+  },
+});
